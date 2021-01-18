7ad21c680049eb088172e6d0c65f89f026623538
Hello <yanjiarui97@126.com>
2021-01-18 18:07:31 -0500

    Add MKL and TBB

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,7 +3,11 @@
 //
 #include "macro.h"
 #include <iostream>
+#include <time.h>
+
+#include <Eigen/Core>
 #include <Eigen/Dense>
+
 
 using Eigen::MatrixXd;
 
@@ -18,5 +22,24 @@ int main(){
     std::cout << m << std::endl;
     std::cout << "Eigen version:" << EIGEN_WORLD_VERSION << "."
               << EIGEN_MAJOR_VERSION << "." << EIGEN_MINOR_VERSION << std::endl;
+
+    //
+    // Test MKL
+    //
+    // Without MKL: 18.7719s
+    // With MKL-SEQ: 7.55845s
+    // With MKL-TBB: 7.30708s
+    srand((unsigned)time(NULL));
+    clock_t start,finish;
+    double totaltime;
+    start=clock();
+
+    Eigen::MatrixXf m1 = Eigen::MatrixXf::Random(7000, 7000);
+    Eigen::MatrixXf m2 = Eigen::MatrixXf::Random(7000, 7000);
+    Eigen::MatrixXf m3 = m1*m2;
+
+    finish=clock();
+    totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
+    std::cout << "The running time of codes above:" << totaltime << "s" << std::endl;
     return 0;
 }
